4ca13dd11f95e11285c6be2d3172d559d0bd71a9
b1tsh1ft3r <26237697+b1tsh1ft3r@users.noreply.github.com>
2021-11-27 20:16:07 -0600

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # tiled2c
-TIled map editor CSV export conversion to C array
+This tool is very simplistic and is a variation of the one written to convert between Tiled map editor CSV exports
+and a format for the sega saturn using JO-engine.
+
+THis tool is written for the purpose of taking CSV exports of tilemaps in Tiled map editor and converting them into
+a C array. THere isnt a bunch of error checking and thus you must enter in the right data to get a valid conversion.
+
+I didnt bother to improve it much because it just works. Feel free to modify or change it to suit your needs.
---
+++ convert.go
@@ -0,0 +1,61 @@
+package main
+
+import "log"
+import "bufio"
+import "encoding/csv"
+import "os"
+import "fmt"
+import "strconv"
+
+func main() {
+    fmt.Println("**************************************");
+    fmt.Println("TILED MAP EDITOR CSV EXPORT TO C ARRAY");
+    fmt.Println("**************************************\n");
+
+    var FILENAME string
+    var TILE_SIZE, HEIGHT_TILES, WIDTH_TILES, MAP_WIDTH, MAP_HEIGHT int
+
+    fmt.Println("Please enter the filename of the exported CSV Tilemap")
+    fmt.Scan(&FILENAME);
+
+    INPUT_FILE, ERR  := os.Open(FILENAME);
+    if ERR != nil { log.Fatal(ERR); }
+
+    fmt.Println("TILE SIZE? (Ex: 8,16,24,32)");
+    fmt.Scan(&TILE_SIZE);
+
+    fmt.Println("MAP HEIGHT IN TILES?");
+    fmt.Scan(&HEIGHT_TILES);
+
+    fmt.Println("MAP WIDTH IN TILES?");
+    fmt.Scan(&WIDTH_TILES);
+
+    MAP_WIDTH=(WIDTH_TILES*TILE_SIZE);
+    MAP_HEIGHT=(HEIGHT_TILES*TILE_SIZE);
+
+    fmt.Println("\n\nCONVERTING TILE MAP...\n");
+
+    CSV_MAP          := csv.NewReader(bufio.NewReader(INPUT_FILE));
+    TILEMAP_DATA, _  := CSV_MAP.ReadAll();  // READ ALL DATA FROM CSV VERSION OF TILEMAP (EXPORTED FROM TILED MAP EDITOR)
+    OUTPUT_FILE, err := os.OpenFile("output.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644);
+    if err != nil { log.Fatal(err); }
+    
+    OUTPUT_FILE.WriteString("const u8 level1["+strconv.Itoa(MAP_WIDTH)+"]["+strconv.Itoa(MAP_HEIGHT)+"] = {\n");
+
+    for i:=0;i<=HEIGHT_TILES-1;i++ {
+
+        for j:=0;j<=WIDTH_TILES-1;j++ {
+
+            tile_value := ((TILEMAP_DATA[i])[j]);
+            if (j == 0) { OUTPUT_FILE.WriteString("     { "+tile_value+", "); continue; }
+            if (j == WIDTH_TILES-1 && i >= HEIGHT_TILES-1) { OUTPUT_FILE.WriteString(tile_value+" }\n};\n"); continue; }            
+            if (j == WIDTH_TILES-1) { OUTPUT_FILE.WriteString(tile_value+" },\n"); continue; }            
+            OUTPUT_FILE.WriteString(tile_value+", ");
+
+        }
+    }
+
+    if err := OUTPUT_FILE.Close(); err != nil { print(err); }
+    fmt.Println("TILEMAP CONVERTED!");
+
+}
